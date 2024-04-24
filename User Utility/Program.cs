b1ab3fa70abb5a908415edf3207ea4246c3c5e12
Mark Bueno <133892804+mk17bu@users.noreply.github.com>
2024-04-24 18:49:25 +0200

    Added creation of posts

--- User Utility/Program.cs
+++ User Utility/Program.cs
@@ -2,7 +2,8 @@
 using UserContext;
 using UserData;
 
-Context context = new();
+Context context = new Context();
+context.Database.EnsureCreated();
 
 while (true)
 {
@@ -16,7 +17,7 @@ while (true)
             CreateUsers();
             break;
         case "2":
-            
+            CreatePosts();
             break;
         case "3":
             
@@ -45,9 +46,15 @@ static void DisplayMenu()
                           "\n\nEnter the corresponding number for your choice (1/2/3/4/5):");
 }
 
+static void ReadLineAndClear()
+{
+    Console.ReadLine();
+    Console.Clear();
+}
+
 void CreateUsers()
 {
-    List<User> userList = new List<User>
+    var userList = new List<User>
     {
         new User { FirstName = "William", LastName = "Turner", Role = Role.Admin },
         new User { FirstName = "Emma", LastName = "Thompson ", Role = Role.Moderator },
@@ -60,6 +67,24 @@ void CreateUsers()
     context.SaveChanges();
 
     Console.WriteLine("Users created \n");
-    Console.ReadLine();
-    Console.Clear();
+    ReadLineAndClear();
+}
+
+void CreatePosts()
+{
+    var userList = context.Users.ToList();
+    var postList = new List<Post>
+    {
+        new Post { Title = "De Jong out for season, back for Euros", Description = "Frenkie de Jong will miss the last six games of Barcelona's season after sustaining another ankle injury in Sunday's 3-2 Clásico defeat against Real Madrid", PostingDate = new DateTime(2024, 4, 22), Author = userList[0]},
+        new Post { Title = "Pochettino on Chelsea thrashing: 'We gave up'", Description = "Mauricio Pochettino admitted Chelsea gave up in Tuesday's 5-0 thrashing at Arsenal, but sought to defend his players by insisting some of the game's greats had similar off days", PostingDate = new DateTime(2024, 4, 23), Author = userList[0]},
+        new Post { Title = "Leverkusen's unbeaten run reaches 45 games", Description = "Bayer Leverkusen's Josip Stanisic scored a stoppage-time goal for the champions to rescue a 1-1 Bundesliga draw at Borussia Dortmund", PostingDate = new DateTime(2024, 4, 21), Author = userList[1]},
+        new Post { Title = "Verstappen eases to China win, Norris second ahead of Perez", Description = "Max Verstappen overcame two different Safety Car periods to record another easy win at the Chinese Grand Prix", PostingDate = new DateTime(2024, 4, 21), Author = userList[2]},
+        new Post { Title = "LeBron sounds off on officiating, walks out of news conference", Description = "LeBron James complains about the officiating in Lakers vs. Nuggets and even mentions the 76ers-Knicks game", PostingDate = new DateTime(2024, 4, 23), Author = userList[3]}
+    };
+
+    context.Posts.AddRange(postList);
+    context.SaveChanges();
+
+    Console.WriteLine("Posts created \n");
+    ReadLineAndClear();
 }
--- UserData/Post.cs
+++ UserData/Post.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Author { get; set; }
-        public DateTime CreationDate { get; set; }
+        public DateTime PostingDate { get; set; }
+        public User Author { get; set; }
     }
 }
